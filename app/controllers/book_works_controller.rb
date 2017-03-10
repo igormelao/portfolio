8144a0c418e248b341fa32bb210689c01bbd103f
Igor Melao <igormelao@gmail.com>
2017-03-10 20:26:10 -0300

    Implemented edit and update funcionality

--- app/controllers/book_works_controller.rb
+++ app/controllers/book_works_controller.rb
@@ -1,4 +1,6 @@
 class BookWorksController < ApplicationController
+  before_action :set_bookwork, only: [:show, :edit, :update]
+
   def index
     @bookWorks = BookWork.all
   end
@@ -16,13 +18,27 @@ class BookWorksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @bookwork.update(bookwork_params)
+      redirect_to book_works_path, notice: "Your bookwork was updated with sucessfully"
+    else
+      render :edit
+    end
+  end
+
   def show
-    @bookwork = BookWork.find(params[:id])
   end
 
   private
 
-  def bookwork_params
-    params.require(:book_work).permit(:title, :subtitle, :body)
-  end
+    def set_bookwork
+      @bookwork = BookWork.find(params[:id])
+    end
+
+    def bookwork_params
+      params.require(:book_work).permit(:title, :subtitle, :body)
+    end
 end
