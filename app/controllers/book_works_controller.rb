93159e3b753ce7121bf9ae5da8d21a926b556d87
Igor Melao <igormelao@gmail.com>
2017-03-10 20:12:33 -0300

    Integrated new, show and create actions for woorkbooks

--- app/controllers/book_works_controller.rb
+++ app/controllers/book_works_controller.rb
@@ -2,4 +2,27 @@ class BookWorksController < ApplicationController
   def index
     @bookWorks = BookWork.all
   end
+
+  def new
+    @bookwork = BookWork.new
+  end
+
+  def create
+    @bookwork = BookWork.new(bookwork_params)
+    if @bookwork.save
+      redirect_to book_works_path, notice: "Your bookwork was created with sucessfully"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @bookwork = BookWork.find(params[:id])
+  end
+
+  private
+
+  def bookwork_params
+    params.require(:book_work).permit(:title, :subtitle, :body)
+  end
 end
