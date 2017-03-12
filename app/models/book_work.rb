da799e0db70f03c638f70b1d5fd6b165d56725c4
Igor Melao <igormelao@gmail.com>
2017-03-12 16:21:54 -0300

    Implemented image defaults for bookwork items

--- app/models/book_work.rb
+++ app/models/book_work.rb
@@ -6,4 +6,13 @@ class BookWork < ApplicationRecord
   end
 
   scope :ruby_on_rails_bookwork_items, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults
+
+  private
+
+    def set_defaults
+      self.main_image  ||= "http://placehold.it/600x400"
+      self.thumb_image ||= "http://placehold.it/350x200"
+    end
 end
