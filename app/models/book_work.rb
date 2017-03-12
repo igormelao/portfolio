b083c47f99cddbf518e727168448023a4aa58dca
Igor Melao <igormelao@gmail.com>
2017-03-12 18:08:18 -0300

    Implemented concern for managing image data

--- app/models/book_work.rb
+++ app/models/book_work.rb
@@ -1,4 +1,6 @@
 class BookWork < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -12,7 +14,7 @@ class BookWork < ApplicationRecord
   private
 
     def set_defaults
-      self.main_image  ||= "http://placehold.it/600x400"
-      self.thumb_image ||= "http://placehold.it/350x200"
+      self.main_image  ||= Placeholder.image_generator('600', '400')
+      self.thumb_image ||= Placeholder.image_generator('350', '200')
     end
 end
